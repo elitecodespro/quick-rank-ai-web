1e25015b7539977b3da3c9dd8d11528e6cf03547
elitecodespro <elitevsaservices@gmail.com>
2024-11-06 06:48:21 +0400

    Fixed auth pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,15 +1,15 @@
 import SignInForm from "@/components/auth/signin-form";
 import { Icons } from "@/components/marketing/global/icons";
+import Image from "next/image";
 import Link from "next/link";
 
 const SignInPage = () => {
     return (
-        <div className="flex flex-col items-start max-w-sm mx-auto h-dvh overflow-hidden pt-4 md:pt-20">
+        <div className="flex flex-col items-start max-w-sm mx-auto h-dvh pt-4 md:pt-20">
             <div className="flex items-center w-full py-8 border-b border-border/80">
                 <Link href="/#home" className="flex items-center gap-x-2">
-                    <Icons.logo className="w-6 h-6" />
                     <h1 className="text-lg font-medium">
-                        linkify
+                        <Image src={'/assets/logo.png'} alt="Quick Rank Logo" width={200} height={200} />
                     </h1>
                 </Link>
             </div>
@@ -31,7 +31,7 @@ const SignInPage = () => {
             <div className="flex items-start mt-auto border-t border-border/80 py-6 w-full">
                 <p className="text-sm text-muted-foreground">
                     Don&apos;t have an account?{" "}
-                    <Link href="/auth/sign-up" className="text-primary">
+                    <Link href="/sign-up" className="text-primary">
                         Sign up
                     </Link>
                 </p>
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,15 +1,15 @@
 import SignUpForm from "@/components/auth/signup-form";
 import { Icons } from "@/components/marketing/global/icons";
+import Image from "next/image";
 import Link from "next/link";
 
 const SignUpPage = () => {
     return (
-        <div className="flex flex-col items-start max-w-sm mx-auto h-dvh overflow-hidden pt-4 md:pt-20">
+        <div className="flex flex-col items-start max-w-sm mx-auto h-dvh pt-4 md:pt-20 mb-10">
             <div className="flex items-center w-full py-8 border-b border-border/80">
                 <Link href="/#home" className="flex items-center gap-x-2">
-                    <Icons.logo className="w-6 h-6" />
                     <h1 className="text-lg font-medium">
-                        Quick Rank AI
+                        <Image src={'/assets/logo.png'} alt="Quick Rank Logo" width={200} height={200} />
                     </h1>
                 </Link>
             </div>
@@ -31,7 +31,7 @@ const SignUpPage = () => {
             <div className="flex items-start mt-auto border-t border-border/80 py-6 w-full">
                 <p className="text-sm text-muted-foreground">
                     Already have an account?{" "}
-                    <Link href="/auth/sign-in" className="text-primary">
+                    <Link href="/sign-in" className="text-primary">
                         Sign in
                     </Link>
                 </p>
